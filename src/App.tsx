826e1ccca40abf01d9ebfc6d142e9ae720079d26
廖天佑（Bless Liao） <bless@25min.co>
2026-04-02 11:14:18 +0800

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ScrollToTop } from './components/ScrollToTop';
 // Pages
 import { HomePage } from './pages/HomePage';
 import { PainPointPage } from './pages/PainPointPage';
@@ -11,6 +12,7 @@ import { ContractPage } from './pages/ContractPage';
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/path/:painId" element={<PainPointPage />} />
