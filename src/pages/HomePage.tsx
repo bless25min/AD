8986e3e2330f9df4fa805052bffb354e5337e6c1
廖天佑（Bless Liao） <bless@25min.co>
2026-05-11 17:28:13 +0800

    update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,7 +14,7 @@ export const HomePage = () => {
 
   const cases = [
     { img: "/images/dr-julia-hero-banner.png", title: "醫美術後模擬", href: "https://dr-julia.25min.co/" },
-    { img: "/images/遊戲化落地頁.jpg", title: "遊戲化機制", href: "https://soyaplayablead.25min.co/ad" },
+    { img: "/images/playablead.png", title: "遊戲化機制", href: "https://soyaplayablead.25min.co/ad" },
     { img: "/images/soya-happiness-hero.jpg", title: "知識解鎖", href: "https://soya.massenlighten.com/" },
     { img: "/images/fightnight-hero-poster.png", title: "健身互動", href: "https://fightnight.25min.co/" },
     { img: "/images/tolokah-adult-assessment.png", title: "身高體態評估", href: "https://tolokah.25min.co/" },
